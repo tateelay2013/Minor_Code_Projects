b96b33cd4e384a08d25421488fc9bfce3416f515
Thant Synn Win <tateelay2013@gmail.com>
2020-12-02 15:55:10 +1100

    Update DNA_Base_Complementary.py

--- Python/DNA_Base_Complementary.py
+++ Python/DNA_Base_Complementary.py
@@ -1,11 +1,13 @@
 #DNA base complementary
 bases = input("Insert a sample DNA strand: ")
+
+for base in bases:
+    if base != "C" and base != "T" and base != "A" and base != "G":
+        print("Invalid Input")
+        exit()
+
 for base in bases:
     if base == "C": print("G", end="")
     elif base == "T": print("A", end="")
     elif base == "A": print("T", end="")
     elif base == "G": print("C", end="")
-    else: 
-        bases = False
-        print("Invalid Input") 
-        break
